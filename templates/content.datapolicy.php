26cf1470727b83d9b0177353667b77f99e029307
Joscha Jaeger <joscha.jaeger@gmail.com>
2020-06-04 12:15:48 +0200

    upgrade data policy

--- templates/content.datapolicy.php
+++ templates/content.datapolicy.php
@@ -36,8 +36,6 @@
 		<div class="row m-0" style="position: relative; z-index: 1">
 			<div class="col-lg-10 mx-auto">
 				<h2><?php echo L::dataPolicy; ?> (DE)</h2>
-				<p>
-				Die für die Datenerhebung, -verarbeitung und -nutzung verantwortliche Stelle ist der&nbsp;<a href="https://okfn.de">Open Knowledge Foundation Deutschland e.V.</a>, Singerstraße 109, 10179 Berlin. Anfragen an unsere Datenschutzbeauftragte Beata Hubrig können gesendet werden an datenschutz@okfn.de. Zuständige Aufsichtsbehörde ist die <a href="https://www.datenschutz-berlin.de/ueber-uns/kontakt/">Berliner Beauftragte für Datenschutz und Informationsfreiheit</a>.</p>
 				<p>Die Sicherheit Ihrer Daten hat bei uns höchste Priorität. Wir ergreifen jede nur mögliche Maßnahme, um Ihre persönlichen Daten vor unbefugtem Zugriff zu schützen. Sämtliche Anforderungen der EU-Datenschutzgrundverordnung (DSGVO), des Bundesdatenschutzgesetzes (BDSG), des Telemediengesetzes (TMG) sowie alle weiteren datenschutzrechtlichen Vorschriften werden von uns eingehalten. Sie können unsere Seite besuchen, ohne Angaben zu Ihrer Person zu machen.</p>
 				<p>Auf der Webseite OpenParliament.TV sowie den parlamentspezifischen Unterseiten (z.B. de.openparliament.tv) können Sie Reden im Deutschen Bundestag in Kombination mit den Parlamentsprotokollen und zusätzlichen Informationen (relevante Drucksachen, Abgeordnetenprofile auf abgeordnetenwatch.de) abrufen und durchsuchen.</p>
 				<hr>
@@ -61,8 +59,8 @@
 				<p>Die Erhebung von personenbezogenen Informationen kann durch die Nutzung von Matomo und der Kürzung der IP-Adresse nahezu vollständig vermieden werden. Eine Rückführung dieser Daten auf einzelne Personen ist in einzelfällen Fällen jedoch zumindest theoretisch nicht ganz auszuschließen.</p>
 				<hr>
 				<h3>2. Übermittlung von Daten an Dritte</h3>
-				<h4>2.1. Übermittlung im Rahmen der <b>[ABGEORDNETENWATCH, BUNDESTAG, VIDEODIENSTLEISTER]</b></h4>
-				<p><b>[BESCHREIBUNG]</b></p>
+				<h4>2.1. Übermittlung im Rahmen der [ABGEORDNETENWATCH, BUNDESTAG, VIDEODIENSTLEISTER]</h4>
+				<p>[BESCHREIBUNG (noch nicht relevant)]</p>
 				<h4>2.2. Keine Übermittlung an Dritte zu Werbezwecken ohne ausdrückliche Einwilligung</h4>
 				<p>Eine Weitergabe Ihrer personenbezogenen Daten an Dritte erfolgt grundsätzlich nur, sofern dies zur Erbringung unserer Dienstleistung oder zur Vertragsdurchführung notwendig ist oder Sie ausdrücklich eingewilligt haben. Behörden oder anderen staatlichen Institutionen werden nur im Rahmen der gesetzlichen Informationspflichten oder aufgrund gerichtlicher Entscheidungen Auskunft erteilt.</p>
 				<h4>2.3 Auskunftsanordnungen</h4>
@@ -81,7 +79,7 @@
 				<h3>5. Sicherheitshinweis</h3>
 				<p>Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.</p>
 				<hr>
-				<p>Stand 4. Februar 2020</p>
+				<p>Stand 4. Juni 2020</p>
 			</div>
 		</div>
 	</main>
