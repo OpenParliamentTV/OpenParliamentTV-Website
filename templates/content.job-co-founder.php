71e596c3b44b05c5be9321129f5338fcda37bb7e
Joscha Jaeger <joscha.jaeger@gmail.com>
2024-09-12 17:27:37 +0200

    add job ad

---
+++ templates/content.job-co-founder.php
@@ -0,0 +1,200 @@
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta charset="utf-8">
+	<meta http-equiv="cache-control" content="no-cache">
+	<meta http-equiv="Expires" content="-1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<title>Job | Co-Founder: Business Development - Open Parliament TV</title>
+
+	<meta itemprop="name" content="<?php echo L::brand; ?>">
+    <meta name="twitter:title" content="Job | Co-Founder: Business Development - <?php echo L::brand; ?>">
+    <meta property="og:title" content="Job | Co-Founder: Business Development - <?php echo L::brand; ?>">
+
+    <meta name="description" content="We are looking for an experienced co-founder and business developer who proactively contributes to the project and plays a key role in shaping the future of digital democracy with us.">
+    <meta itemprop="description" content="We are looking for an experienced co-founder and business developer who proactively contributes to the project and plays a key role in shaping the future of digital democracy with us.">
+    <meta name="twitter:description" content="We are looking for an experienced co-founder and business developer who proactively contributes to the project and plays a key role in shaping the future of digital democracy with us.">
+    <meta property="og:description" content="We are looking for an experienced co-founder and business developer who proactively contributes to the project and plays a key role in shaping the future of digital democracy with us.">
+
+    <meta itemprop="image" content="https://openparliament.tv/client/images/job-co-founder.png">
+    <meta name="twitter:image" content="https://openparliament.tv/client/images/job-co-founder.png">
+    <meta property="og:image" content="https://openparliament.tv/client/images/job-co-founder.png">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="og:type" content="article">
+
+	<link rel="icon" type="image/png" href="favicon.png" />
+	
+	<link type="text/css" rel="stylesheet" href="client/css/bootstrap.min.css" media="all" />
+	<link type="text/css" rel="stylesheet" href="client/css/Chart.min.css" media="all" />
+	<link type="text/css" rel="stylesheet" href="client/css/frametrail-webfont.css" media="all" />
+	<link type="text/css" rel="stylesheet" href="client/css/style.css" media="all" />
+	<!-- Matomo -->
+	<script>
+	  var _paq = window._paq = window._paq || [];
+	  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+	  _paq.push(['trackPageView']);
+	  _paq.push(['enableLinkTracking']);
+	  (function() {
+	    var u="//stats.openparliament.tv/";
+	    _paq.push(['setTrackerUrl', u+'matomo.php']);
+	    _paq.push(['setSiteId', '2']);
+	    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+	    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+	  })();
+	</script>
+	<!-- End Matomo Code -->
+</head>
+<body>
+	<?php include_once('header.php'); ?>
+	<main style="margin-top: 60px;">
+		<div class="container mb-0">
+			<div class="row m-0" style="position: relative; z-index: 1;">
+				<div class="col-lg-9 mx-auto">
+					<div class="alert alert-warning">This position is not open anymore. The application process has ended.</div>
+					<div style="opacity: .4">
+						<p>We're looking for a </p>
+						<h1 style="font-size: 30px; margin-bottom: 30px;">Co-Founder: <br>Business Development <span style="font-size: 20px"> (m/f/x)</span></h1>
+					</div>
+				</div>
+			</div>
+		</div>
+		<section style="opacity: .4;">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-9 mx-auto card px-1">
+						<div class="card-body">
+							<p>Open Parliament TV has ambitious goals for <a href="https://openparliament.tv/scaling-concept" target="_blank">scaling and further development over the next four years</a>. As part of this process, we aim to <b>permanently divide the project leadership and management of Open Parliament TV between two co-founders</b>, and close gaps in expertise in the areas of business and product development. </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section style="opacity: .4;">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-9 mx-auto card px-1">
+						<div class="card-body">
+							<h2>What is Open Parliament TV?</h2>
+							<p>Open Parliament TV is a <b>search engine and interactive video platform for parliamentary debates</b>. Our vision is to <b>make parliamentary discourse accessible beyond the boundaries of single parliaments</b> and across different levels (municipal, regional, national) and languages. </p>
+							<p>We are a small team consisting of the Founder & Project Lead and, depending on the funding situation, several freelancers and volunteers. Organisationally, we are embedded in the Center for the Cultivation of Technology gGmbH. As part of the scaling process, we are planning to <b>establish ourselves as an independent organisation with a long-term team and you as co-founder</b>. </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section style="opacity: .4;">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-9 mx-auto card px-1">
+						<div class="card-body">
+							<h2>Who are we looking for?</h2>
+							<p>As the Founder & Project Lead, I’m looking for an <b>experienced co-founder</b> who <b>proactively contributes to the project</b> with a great deal of initiative, ideas, and drive and plays a <b>key role in shaping the future</b> of Open Parliament TV. The project has been dependent on me as an individual for too long and it's time to change that! I want to rethink roles, share responsibility and fundamentally reorganise Open Parliament TV. </p>
+							<h3>Your Responsibilities</h3>
+							<ul>
+								<li><b>Business Development and Internationalisation</b> (see <a href="https://openparliament.tv/scaling-concept" target="_blank">Scaling Concept</a>)</li>
+								<ul>
+									<li>Developing a (non-profit) business model </li>
+									<li>Contributing to shaping the long-term vision </li>
+									<li>Product development </li>
+									<li>Developing a scaling and internationalisation strategy </li>
+									<li>Developing a communications strategy for different target groups </li>
+									<li>Communication with stakeholders <br>(establishing partnerships, participation in networks) </li>
+								</ul>
+								<li><b>Operational Management & Team Lead / Development</b> </li>
+								<ul>
+									<li>Managing finances / reporting / controlling </li>
+									<li>Planning budgets </li>
+									<li>Project management </li>
+									<li>International fundraising and grant applications </li>
+									<li>Long-term team development </li>
+									<ul>
+										<li>Definition of roles and responsibilities </li>
+										<li>Recruitment of new team members </li>
+									</ul>
+								</ul>
+							</ul>
+							<h3>Your Qualifications</h3>
+							<ul>
+								<li><b>Minimum 6 years of demonstrated experience</b> in one or more of the following areas </li>
+								<ul>
+									<li>Business development (esp. scaling non-profit organisations ) </li>
+									<li>Founding / company formation </li>
+									<li>Finance management / controlling </li>
+									<li>International fundraising and grant applications </li>
+								</ul>
+								<li><b>Proactive, self-organised and well structured</b> way of working </li>
+								<li>Enthusiasm for <b>strengthening and defending parliamentary democracy</b> </li>
+								<li>Identification with our <a href="https://openparliament.tv/vision-mission-strategy" target="_blank">Vision, Mission and Strategy</a> and our <a href="https://openparliament.tv/code-of-conduct" target="_blank">Code of Conduct</a> </li>
+								<li>Professional communication skills </li>
+								<li>Experience with remote work / leading remote teams </li>
+								<li>Willingness to travel </li>
+								<li>Fluency in English </li>
+							</ul>
+							<p><b>Nice to have</b></p>
+							<ul>
+								<li>Experience in working with (open source) tech projects </li>
+								<li>Experience in the field of open data and open government </li>
+								<li>Experience with (non-profit) start-ups, legal norms </li>
+								<li>Experience working with Notion, Google Workspace, Slack and Github </li>
+								<li>German and / or Dutch language skills </li>
+							</ul>
+							<p>We explicitly <b>welcome applications that do not meet all requirements</b>. We would love to learn more about additional skills, attributes and motivations that are not part of the job description but are relevant to the position. </p>
+							<p>We welcome applications from <b>BPoC</b> (Black and People of Color), individuals with <b>disabilities</b>, people with <b>migration backgrounds</b>, and individuals from other groups <b>underrepresented in technology development</b>. <br>If you belong to these groups, please feel free to mention it in your application. </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section style="opacity: .4;">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-9 mx-auto card px-1">
+						<div class="card-body">
+							<h2>What we offer</h2>
+							<ul>
+								<li><b>Meaningful contribution to shaping digital democracy</b> </li>
+								<li><b>Freedom for self-realisation</b> as co-founder in a young organisation with big potential for scaling and internationalisation </li>
+								<li>Space for you to <b>grow and learn</b> </li>
+								<li>Solid <b>coaching and training</b> budget to support us in the process of organisational development and team building </li>
+							</ul>
+							<ul>
+								<li><b>Full-time position</b> (80-100%), initially limited to 2 years with the prospect of permanent employment / joint foundation </li>
+								<li><b>4-day workweek</b> (100% = 32 hrs) </li>
+								<li><b>Flexible work hours</b> and non-bureaucratic work environment </li>
+								<li>Fully <b>remote</b> (Timezone: CET +- 3h) </li>
+								<li>30 vacation days </li>
+							</ul>
+							<p><b>Compensation transparency </b></p>
+							<p>We have secured funding for this position for two years, with a total annual budget of ~ €68k. Depending on your country of residence and circumstances we can offer full-time employment in Berlin via the Center for the Cultivation of Technology gGmbH or a freelance contract. If we employ you, the annual salary (budget minus employer costs) will amount to ~ €54-58k (100%). </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section style=" margin-bottom: 30px; opacity: .4">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-9 mx-auto card px-1">
+						<div class="card-body">
+							<h2>How to apply</h2>
+							<p>Please send us your application via email to <a href="mailto:jobs@openparliament.tv">jobs@openparliament.tv</a> with </p>
+							<ul>
+								<li>a free form <b>statement of motivation</b> (cover letter, presentation, video, anything else that helps us understand your motivation) </li>
+								<li>your <b>resume</b> </li>
+								<li>your earliest possible <b>start date</b> </li>
+							</ul>
+							<p>We aim for a start date of <b>June 1st</b> and will conduct <b>interviews on a running basis</b> until the position is filled. We will inform you about the status of your application via email. </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
+	<?php include_once('footer.php'); ?>
+	<script type="text/javascript" src="client/js/jquery-3.4.1.min.js"></script>
+	<script type="text/javascript" src="client/js/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="client/js/Chart.min.js"></script>
+	<script type="text/javascript" src="client/js/bootstrap.bundle.min.js"></script>
+	<script type="text/javascript" src="client/js/index.js"></script>
+</body>
